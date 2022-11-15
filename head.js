bf4db4c97915682eee777c921dd0442247fb77af
Hoss Agina <hoss.agina@telus.com>
2022-11-14 17:05:34 -0700

    fixed assertEqual.JS and adding head.js to the repo

--- head.js
+++ head.js
@@ -7,6 +7,14 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+//FUNCTION TO RETURN HEAD OF ARRAY
+const head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  } else {
+    return arr[0];
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
