d1e0afe90e376d2eaa223f2145ab176cac1733a8
Hoss Agina <hoss.agina@telus.com>
2022-11-16 17:17:23 -0700

    letterPositions.js is getting added here

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arr1,arr2) {
+  let isEqual = true;
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i in arr1) {
+    if (arr1[i] !== arr2[i]) {
+      isEqual = false;
+    }
+  }
+  return isEqual;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🪀🪀🪀 Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`👺👺👺 Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let index = 0; index < sentence.length; index++) {
+    const letter = sentence[index];
+    if (letter !== " ") {
+      if (!results[letter]) {
+        results[letter] = [];
+      }
+      results[letter].push(index);
+    }
+  }
+  return results;
+};
+
+
+
+// console.log(letterPositions("lighthouse in the house"));
+// assertArraysEqual(letterPositions("hello").e, [1]);
