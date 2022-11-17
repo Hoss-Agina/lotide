4ba84c18c3e7c63d92669531ef49c837e15ddac3
Hoss Agina <hoss.agina@telus.com>
2022-11-16 17:29:51 -0700

    adding findKeyByValue.js to lotide

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🪀🪀🪀 Assertion Passed ${actual} === ${expected}`);
+  } else {
+    console.log(`👺👺👺 Assertion Failed ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(movieObject, str) {
+  for (let key in movieObject) {
+    if (str === movieObject[key]) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
