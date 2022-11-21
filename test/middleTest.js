a120153bb587d9b99de35a470140e5372f9c2a17
Hoss Agina <hoss.agina@telus.com>
2022-11-21 14:21:37 -0700

    adding all changed files:headTest.js & middleTest.js & tailTest after incorporating mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,18 @@
-assertArraysEqual = require('../assertArraysEqual');
-middle = require('../middle');
+// assertArraysEqual = require('../assertArraysEqual');
+// middle = require('../middle');
 
-actual = middle([1,2,3,4,5]);
-expected = [3];
-assertArraysEqual(actual, expected)
+// actual = middle([1,2,3,4,5]);
+// expected = [3];
+// assertArraysEqual(actual, expected)
+
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns [2,3] for [1,2,3,4]", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,24 @@
-const tail = require("../tail");
-const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
+// const assertEqual = require("../assertEqual");
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-//Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// //Test Case: Check the original array
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns [2,3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns [] for [5]", () => {
+    assert.deepEqual(tail([5]), []); 
+  });
+});
