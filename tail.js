277b12b87982ebe666647a368c34b256f4ef5e2f
Hoss Agina <hoss.agina@telus.com>
2022-11-21 10:14:07 -0700

    added tailTest.js to test folder and adjusted test.js file so function is exported for tailTest.js to be used

--- tail.js
+++ tail.js
@@ -1,4 +1,3 @@
-// FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`🪀🪀🪀 Assertion Passed ${actual} === ${expected}`);
@@ -15,12 +14,4 @@ const tail = function(arr) {
   return newArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-//Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
